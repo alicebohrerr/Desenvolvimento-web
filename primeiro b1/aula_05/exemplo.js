5ecebbb98ae9aa8a2f92a965bbfde8a5523e0653
alicebohrerr <aliceoliveirabohrer@gmail.com>
2026-03-28 09:56:48 -0300

    trabalho aula 05

---
+++ primeiro b1/aula_05/exemplo.js
@@ -0,0 +1,11 @@
+const teste = 20;
+var nome = "alice"
+let idade = 19;
+
+
+console.log(nome, idade + "" + " anos" );
+
+const listaAmigos = ["amigos: " + "antonio", "kendra", "milena"]
+
+
+console.log(listaAmigos);
---
+++ primeiro b1/trabalhoAula5/calculadora.js
@@ -0,0 +1,12 @@
+const valorConta = 100
+const percentualGorjeta = 15 
+
+let valorGorjeta = valorConta * percentualGorjeta / 100;
+
+let ValorTotal = valorConta + valorGorjeta;
+
+
+console.log("Valor da conta: R$"+ valorConta);
+console.log("valor da Gorjeta (15%) : R$"+ valorGorjeta);
+console.log("valor total a ser pago: R$"+ ValorTotal);
+
